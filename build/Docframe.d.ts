f7160e8eb320f3d8c878e21032e07fdf366002a7
Gerald Wagner <git@wagnergerald.com>
2024-04-17 11:32:45 +0200

    Add layout

--- build/Docframe.d.ts
+++ build/Docframe.d.ts
@@ -8609,6 +8609,9 @@ export interface INode {
 
     /** Node rule */
     rule?: (IProtoRule|null);
+
+    /** Node layout */
+    layout?: (IProtoLayout|null);
 }
 
 /** Represents a Node. */
@@ -8761,8 +8764,11 @@ export class Node implements INode {
     /** Node rule. */
     public rule?: (IProtoRule|null);
 
+    /** Node layout. */
+    public layout?: (IProtoLayout|null);
+
     /** Node object. */
-    public object?: ("listSetting"|"colorDef"|"brick"|"template"|"formatted"|"image"|"paragraphFormat"|"textBrick"|"text"|"linebreak"|"spaceVertically"|"footer"|"header"|"table"|"tableRow"|"tableCell"|"cDef"|"pDef"|"applyCDef"|"applyPDef"|"applyPtConfig"|"applyUlConfig"|"ptConfig"|"ulConfig"|"newPage"|"variable"|"namedString"|"paragraph"|"section"|"span"|"link"|"directory"|"tableContentGroup"|"tableConfig"|"tableCellConfig"|"tableRowConfig"|"tableContentGroupConfig"|"brickReference"|"indentation"|"barcode"|"wsArea"|"carryOver"|"subTotal"|"loop"|"loopEntry"|"rule");
+    public object?: ("listSetting"|"colorDef"|"brick"|"template"|"formatted"|"image"|"paragraphFormat"|"textBrick"|"text"|"linebreak"|"spaceVertically"|"footer"|"header"|"table"|"tableRow"|"tableCell"|"cDef"|"pDef"|"applyCDef"|"applyPDef"|"applyPtConfig"|"applyUlConfig"|"ptConfig"|"ulConfig"|"newPage"|"variable"|"namedString"|"paragraph"|"section"|"span"|"link"|"directory"|"tableContentGroup"|"tableConfig"|"tableCellConfig"|"tableRowConfig"|"tableContentGroupConfig"|"brickReference"|"indentation"|"barcode"|"wsArea"|"carryOver"|"subTotal"|"loop"|"loopEntry"|"rule"|"layout");
 
     /**
      * Creates a new Node instance using the specified properties.
@@ -8892,7 +8898,8 @@ export enum NodeType {
     DOCUMENT_ELEMENT_LOOP = 65,
     DOCUMENT_ELEMENT_LOOP_ENTRY = 66,
     DOCUMENT_ELEMENT_RULE = 67,
-    DOCUMENT_ELEMENT_LIST_LEVEL_SETTING = 68
+    DOCUMENT_ELEMENT_LIST_LEVEL_SETTING = 68,
+    DOCUMENT_ELEMENT_LAYOUT = 69
 }
 
 /** Properties of a ProtoImage. */
@@ -11624,6 +11631,109 @@ export class ProtoColorDef implements IProtoColorDef {
 
     /**
      * Gets the default type url for ProtoColorDef
+     * @param [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
+     * @returns The default type url
+     */
+    public static getTypeUrl(typeUrlPrefix?: string): string;
+}
+
+/** Properties of a ProtoLayout. */
+export interface IProtoLayout {
+
+    /** ProtoLayout parent */
+    parent?: (IProtoDocumentElement|null);
+
+    /** ProtoLayout name */
+    name?: (string|null);
+}
+
+/** Represents a ProtoLayout. */
+export class ProtoLayout implements IProtoLayout {
+
+    /**
+     * Constructs a new ProtoLayout.
+     * @param [properties] Properties to set
+     */
+    constructor(properties?: IProtoLayout);
+
+    /** ProtoLayout parent. */
+    public parent?: (IProtoDocumentElement|null);
+
+    /** ProtoLayout name. */
+    public name: string;
+
+    /**
+     * Creates a new ProtoLayout instance using the specified properties.
+     * @param [properties] Properties to set
+     * @returns ProtoLayout instance
+     */
+    public static create(properties?: IProtoLayout): ProtoLayout;
+
+    /**
+     * Encodes the specified ProtoLayout message. Does not implicitly {@link ProtoLayout.verify|verify} messages.
+     * @param message ProtoLayout message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encode(message: IProtoLayout, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Encodes the specified ProtoLayout message, length delimited. Does not implicitly {@link ProtoLayout.verify|verify} messages.
+     * @param message ProtoLayout message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encodeDelimited(message: IProtoLayout, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Decodes a ProtoLayout message from the specified reader or buffer.
+     * @param reader Reader or buffer to decode from
+     * @param [length] Message length if known beforehand
+     * @returns ProtoLayout
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): ProtoLayout;
+
+    /**
+     * Decodes a ProtoLayout message from the specified reader or buffer, length delimited.
+     * @param reader Reader or buffer to decode from
+     * @returns ProtoLayout
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): ProtoLayout;
+
+    /**
+     * Verifies a ProtoLayout message.
+     * @param message Plain object to verify
+     * @returns `null` if valid, otherwise the reason why it is not
+     */
+    public static verify(message: { [k: string]: any }): (string|null);
+
+    /**
+     * Creates a ProtoLayout message from a plain object. Also converts values to their respective internal types.
+     * @param object Plain object
+     * @returns ProtoLayout
+     */
+    public static fromObject(object: { [k: string]: any }): ProtoLayout;
+
+    /**
+     * Creates a plain object from a ProtoLayout message. Also converts values to other types if specified.
+     * @param message ProtoLayout
+     * @param [options] Conversion options
+     * @returns Plain object
+     */
+    public static toObject(message: ProtoLayout, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+    /**
+     * Converts this ProtoLayout to JSON.
+     * @returns JSON object
+     */
+    public toJSON(): { [k: string]: any };
+
+    /**
+     * Gets the default type url for ProtoLayout
      * @param [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
      * @returns The default type url
      */
